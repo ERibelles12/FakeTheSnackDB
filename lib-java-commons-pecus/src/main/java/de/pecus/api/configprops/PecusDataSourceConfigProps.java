870b4d9b3129b2cbc0ef507696aa8ac147cc073b
Luis Enrique Sanchez Santiago <luis.sanchez@proa.com.mx>
2025-06-01 22:45:36 -0600

    Se eliminan proyectos y archivos que ya no se utilizan

--- lib-java-commons-pecus/src/main/java/de/pecus/api/configprops/PecusDataSourceConfigProps.java
+++
@@ -1,8 +0,0 @@
-package de.pecus.api.configprops;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "spring.datasource")
-public class PecusDataSourceConfigProps extends SpringDataSourceConfigProps {
-
-}
--- lib-java-commons-pecus/src/main/java/de/pecus/api/encryption/exceptions/InputSizeMismatchException.java
+++
@@ -1,8 +0,0 @@
-package de.pecus.api.encryption.exceptions;
-
-public class InputSizeMismatchException extends IllegalArgumentException{
-    public InputSizeMismatchException(String message) {
-        System.err.println("Input must be divisible into 128 bit blocks.");
-        System.err.println(message);
-    }
-}
